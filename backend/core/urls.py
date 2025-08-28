6d619bddca355893fc843ba08bc7ffa8c5452746
dgimenezdeveloper <dgimenez.developer@gmail.com>
2025-08-28 08:57:34 -0300

    Vista de bienvenida, endpoints visibles, limpieza y preparación para pruebas integrales

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,13 @@
+
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views_home import home
+
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
 ]
